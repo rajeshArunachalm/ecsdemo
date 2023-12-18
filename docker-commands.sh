a715b215cb254eb34b1ec982eb65866f85d8b1d0
RAJESH <rajeshdevops.24.04.93@gmail.com>
2023-12-18 11:45:25 +0530

    docker commands file

---
+++ docker-commands.sh
@@ -0,0 +1,35 @@
+docker -v  
+
+docker version  
+
+docker system info
+
+docker images
+
+docker build . -t dockerimagename:tab
+ex: docker build . -t nginximage:1.0
+
+docker run --name containername -d -p 9004:80 dockerimage:tag
+-d is it will run the container as backgrond
+-p means 9004 is port which we access from outside the docker and actuall application running on 80 which is inside of the docker.
+ex:
+docker run --name webs -d -p 9004:80 nginximage:1.0
+docker ps
+
+docker ps -a
+
+docker logs containername or containerID
+
+docker logs containername or containerID -f
+
+docker exec -it containername or containerID bash [ this command brings you inside the container ]
+
+docker pull
+
+docker login
+enter user name
+enter password
+
+docker pause imagename:tag
+
+
